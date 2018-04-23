f786a4e8666ab1458d7c2a1d84e834ee0d690019
tommont <businessmontgomery@gmail.com>
2018-04-23 16:48:24 -0500

    First Lesson

---
+++ Codecademy JS Lesson 1.js
@@ -0,0 +1,36 @@
+Log Testing:
+console.log("Hello!");
+console.log("Die Hard");
+
+Data Types:
+console.log('JavaScript');
+console.log(33.7);
+console.log(true);
+console.log(null);
+
+Operator Types:
+console.log(32 + 3.5);
+console.log(2018-1969);
+console.log(65/240);
+console.log(.2708*100);
+
+console.log('Teaching the world how to code'.length);
+
+// Log Codecademy in all uppercase letters
+console.log('Codecademy'.toUpperCase()); 
+
+// Use a string method to log the following statment without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim()); 
+
+console.log(Math.random()*100)
+console.log(Math.floor(Math.random()*100))
+console.log(Math.ceil(43.8))
+console.log(Number.isInteger(2017))
+
+//Single Line Comment
+
+/*
+multiple
+line
+comment
+*/
